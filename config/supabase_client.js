beb86cb1408f89c697f250e7540b18592d93edec
HazWahb8080 <hazem.wahb100@gmail.com>
2024-07-05 00:31:37 +0300

    fix2s

--- config/supabase_client.js
+++ config/supabase_client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://yxhdblivxbrszkbcciqb.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4aGRibGl2eGJyc3prYmNjaXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczOTE4NzksImV4cCI6MjAzMjk2Nzg3OX0.DF3fJe6AFR0N-owQRfM3-mWD5cfFf_u-tolpFVFUsSo`
 export const supabase = createClient(supabaseUrl, supabaseKey)
